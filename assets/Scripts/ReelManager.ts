efb98e6f3b8feb2c9595944646909261ba94ef37
tzuhao.wang <tzuhao.wang@MacBook-Air-2>
2026-09-11 14:08:25 +0800

    Initial

---
+++ assets/Scripts/ReelManager.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+import { GameManager } from './GameManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('ReelManager')
+export class ReelManager extends Component {
+    start() {
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
